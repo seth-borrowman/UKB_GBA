b80213ca5d753c34871f34b54564e3ed5b8e1804
Seth Borrowman <87055552+seth-borrowman@users.noreply.github.com>
2023-09-14 16:35:40 -0500

    Add error handling for model

--- ParkinsonManhattan.R
+++ ParkinsonManhattan.R
@@ -5,6 +5,7 @@ library(logistf)
 
 setwd("~/LSS/lss_chndr/UKB Research/GBA1")
 
+### Set up workspace ----
 covariate <- read_csv("PheWAS/covariates.csv")
 plinkPath <- read_csv("plinkPath.csv")
 phenotypes <- read_csv("PheWAS/phecode.csv")
@@ -13,6 +14,7 @@ phenotypes <- phenotypes[,-1]
 excl_codes <- PheWAS::phecode_exclude$exclusion_criteria[which(PheWAS::phecode_exclude$code == 332)]
 excl <- which(colnames(phenotypes) %in% excl_codes)
 
+### Set exclusions to match PheWAS package----
 excl_phecode <- phenotypes[,c(1, excl)] %>% filter_all(any_vars(. %in% c(T)))
 phenotypes <- phenotypes %>%
     mutate(Parkinson = case_when(
@@ -28,9 +30,33 @@ phenotypes <- phenotypes %>%
 
 summary <- data.frame()
 
+### Create functions for tryCatch ----
+tryfun <- function(data, var_name) {
+    mod <- logistf(Parkinson ~ ., data = data,
+                   control = logistf.control(maxstep = -1))
+    beta <- unname(mod$coefficients[24])
+    beta_ci_low <- unname(mod$ci.lower[24])
+    beta_ci_upper <- unname(mod$ci.upper[24])
+    OR <- exp(beta)
+    OR_ci_low <- exp(beta_ci_low)
+    OR_ci_upper <- exp(beta_ci_upper)
+    pval <- unname(mod$prob[24])
+    sum_line <- c(var_name, beta, beta_ci_low, beta_ci_upper,
+                  OR, OR_ci_low, OR_ci_upper, pval)
+    return(sum_line)
+}
+errorfun <- function(var_name) {
+    sum_line <- c(var_name, rep(1, 7))
+    return(sum_line)
+}
+
+### Test association of each variant ----
 for (i in 2:ncol(plinkPath)) {
     genotype <- plinkPath[,c(1, i)]
     varname <- colnames(genotype)[2]
+    message(paste(varname,
+                  as.character(as.integer(i) - 1),
+                  "/", ncol(plinkPath) - 1))
     colnames(genotype)[2] <- "Variant"
     genotype <- genotype %>%
         mutate(Variant = case_when(
@@ -45,34 +71,34 @@ for (i in 2:ncol(plinkPath)) {
         mutate(Alcohol = factor(Alcohol)) %>%
         mutate(TBI = factor(TBI)) %>%
         mutate(Parkinson = factor(Parkinson))
-    mod <- logistf(Parkinson ~ ., data = df,
-                   control = logistf.control(maxstep = -1))
-    beta <- unname(mod$coefficients[24])
-    beta_ci_low <- unname(mod$ci.lower[24])
-    beta_ci_upper <- unname(mod$ci.upper[24])
-    OR <- exp(beta)
-    OR_ci_low <- exp(beta_ci_low)
-    OR_ci_upper <- exp(beta_ci_upper)
-    pval <- unname(mod$prob[24])
-    sum_line <- c(varname, beta, beta_ci_low, beta_ci_upper,
-                  OR, OR_ci_low, OR_ci_upper, pval)
+    message("Fitting model")
+    # Some have had trouble converging - use try/catch
+    tryCatch(sum_line <- tryfun(df, varname),
+             error = (sum_line <- errorfun(varname)))
+    print(sum_line)
     summary <- rbind(summary, sum_line)
 }
+# Rename summary columns something useful
 colnames(summary) <- c("variant", "beta", "beta_ci_low", "beta_ci_upper",
                        "OR", "OR_ci_low", "OR_ci_upper", "pval")
+
+### Set significance cutoffs using Bonferroni and Bejamini-Hochberg FDR ----
 summary <- summary %>%
     mutate(Bonferroni = if_else(pval <= (0.05/nrow(summary)), T, F)) %>%
-    mutate(FDR = if_else (pval <= ((match(pval,
-                                          summary$pval[order(summary$pval, decreasing = F)]) /
-                                        nrow(summary)) * 0.05), T, F))
+    mutate(FDR = if_else(pval <= ((match(pval,
+                                         summary$pval[order(summary$pval, decreasing = F)]) /
+                                       nrow(summary)) * 0.05), T, F))
 max_FDR_p <- max(summary[which(summary$FDR == T), 8])
 summary <- summary %>%
     mutate(FDR = if_else(summary$pval <= max_FDR_p, T, F))
 
+### Write to file ----
 write_csv(summary, "ManhattanSummary.csv")
 
+### Plot ----
+# Get variant position
 summary <- merge(summary, pathVars[,c(1,3)], by.x = "variant", by.y = "Variant")
-
+# Manhattan plot
 ggplot(summary, aes(x = Pos, y = -log10(pval))) +
     geom_point() +
     geom_hline(aes(yintercept = -log10(0.05/nrow(summary)),
