9e58ff5c7c3cade18f5d2b914f22efa15bf29a14
Seth Borrowman <87055552+seth-borrowman@users.noreply.github.com>
2023-09-19 09:48:03 -0500

    Use previous computations

--- ParkinsonManhattan.R
+++ ParkinsonManhattan.R
@@ -1,120 +1,55 @@
 library(tidyverse)
 library(ggrepel)
 library(latex2exp)
-library(logistf)
 
-setwd("~/LSS/lss_chndr/UKB Research/GBA1")
+
 
 ### Set up workspace ----
-covariate <- read_csv("PheWAS/covariates.csv")
-plinkPath <- read_csv("plinkPath.csv")
-phenotypes <- read_csv("PheWAS/phecode.csv")
+setwd("Z:/UKB Research/GBA1/")
 pathVars <- read_csv("pathVars.csv")
-phenotypes <- phenotypes[,-1]
-excl_codes <- PheWAS::phecode_exclude$exclusion_criteria[which(PheWAS::phecode_exclude$code == 332)]
-excl <- which(colnames(phenotypes) %in% excl_codes)
+filelist <- list.files(path = "PheWAS/csv/")
 
-### Set exclusions to match PheWAS package----
-excl_phecode <- phenotypes[,c(1, excl)] %>% filter_all(any_vars(. %in% c(T)))
-phenotypes <- phenotypes %>%
-    mutate(Parkinson = case_when(
-        `332` == T  ~ 1,
-        .default = 0
-    )) %>%
-    mutate(Parkinson = case_when(
-        id %in% excl_phecode$id ~ 0,
-        .default = Parkinson
-    )) %>%
-    select(c(id, Parkinson)) %>%
-    rename(IID = id)
-
+### Pull p-val from files ----
 summary <- data.frame()
-
-### Create function for tryCatch ----
-tryfun <- function(data, var_name) {
-    mod <- logistf(Parkinson ~ ., data = data,
-                   control = logistf.control(maxstep = -1))
-    beta <- unname(mod$coefficients[24])
-    beta_ci_low <- unname(mod$ci.lower[24])
-    beta_ci_upper <- unname(mod$ci.upper[24])
-    OR <- exp(beta)
-    OR_ci_low <- exp(beta_ci_low)
-    OR_ci_upper <- exp(beta_ci_upper)
-    pval <- unname(mod$prob[24])
-    sum_line <- c(var_name, beta, beta_ci_low, beta_ci_upper,
-                  OR, OR_ci_low, OR_ci_upper, pval)
-    return(sum_line)
+for (file in filelist) {
+    currentfile <- read_csv(paste("PheWAS/csv/", file, sep = ""))
+    park <- currentfile[which(currentfile$phenotype == "332"),]
+    if (substr(file, 1, 1) == "1") {
+        snp <- strsplit(file, split = "_")
+        snp <- paste(snp[[1]][1], ":", snp[[1]][2], ":",
+                     snp[[1]][3], ">", snp[[1]][4], sep = "")
+        park$snp <- snp
+    }
+    newline <- merge(park, pathVars,
+                     by.x = "snp", by.y = "Variant")
+    summary <- rbind(summary, newline)
 }
-
-### Test association of each variant ----
-for (i in 2:ncol(plinkPath)) {
-    genotype <- plinkPath[,c(1, i)]
-    varname <- colnames(genotype)[2]
-    message(paste(varname,
-                  as.character(as.integer(i) - 1),
-                  "/", ncol(plinkPath) - 1))
-    colnames(genotype)[2] <- "Variant"
-    genotype <- genotype %>%
-        mutate(Variant = case_when(
-            Variant == 2 ~ 0,
-            Variant == 1 | Variant == 0 ~ 1
-        )) %>%
-        mutate(Variant = factor(Variant))
-    df <- merge(covariate, phenotypes) %>%
-        merge(., genotype) %>%
-        select(-IID) %>%
-        mutate(Sex = factor(Sex)) %>%
-        mutate(Alcohol = factor(Alcohol)) %>%
-        mutate(TBI = factor(TBI)) %>%
-        mutate(Parkinson = factor(Parkinson))
-    message(Sys.time())
-    message("Fitting model")
-    # Some have had trouble converging - use try/catch
-    sum_line <- tryCatch({tryfun(df, varname)},
-                         error = function(e) {
-                             sum_line <- c(varname, rep(1, 7))
-                             return(sum_line)})
-    print(sum_line)
-    summary <- rbind(summary, sum_line)
-}
-# Rename summary columns something useful
-colnames(summary) <- c("variant", "beta", "beta_ci_low", "beta_ci_upper",
-                       "OR", "OR_ci_low", "OR_ci_upper", "pval")
-
-### Set significance cutoffs using Bonferroni and Bejamini-Hochberg FDR ----
 summary <- summary %>%
-    mutate(Bonferroni = if_else(pval <= (0.05/nrow(summary)), T, F)) %>%
-    mutate(FDR = if_else(pval <= ((match(pval,
-                                         summary$pval[order(summary$pval, decreasing = F)]) /
-                                       nrow(summary)) * 0.05), T, F))
-max_FDR_p <- max(summary[which(summary$FDR == T), 8])
-summary <- summary %>%
-    mutate(FDR = if_else(summary$pval <= max_FDR_p, T, F))
+    rename(pval = p) %>%
+    rename(Variant = snp)
+
+bon <- (0.05 / 214) / nrow(currentfile)
 
 ### Write to file ----
 write_csv(summary, "ManhattanSummary.csv")
 
 ### Plot ----
-# Get variant position
-summary <- merge(summary, pathVars[,c(1,3)], by.x = "variant", by.y = "Variant")
 # Manhattan plot
 ggplot(summary, aes(x = Pos, y = -log10(pval))) +
     geom_point() +
-    geom_hline(aes(yintercept = -log10(0.05/nrow(summary)),
-                   color = "Bonferroni", linetype = "Bonferroni")) +
-    geom_hline(aes(yintercept = -log10(max_FDR_p), color = "FDR",
-                   linetype = "FDR")) +
-    scale_color_manual(values = c("Bonferroni" = "red", "FDR" = "blue"),
+    geom_hline(aes(yintercept = -log10(bon),
+                   color = "Bonferroni adjusted alpha",
+                   linetype = "Bonferroni adjusted alpha")) +
+    scale_color_manual(values = c("Bonferroni adjusted alpha" = "red"),
                        name = "Legend") +
-    scale_linetype_manual(values = c("Bonferroni" = "solid", "FDR" = "dashed"),
+    scale_linetype_manual(values = c("Bonferroni adjusted alpha" = "solid"),
                           name = "Legend") +
     geom_label_repel(
-        data = . %>% mutate(label = ifelse((Bonferroni == T) | (FDR == T),
-                                           as.character(variant), "")), aes(label = label), size = 3.5,
+        data = . %>% mutate(label = ifelse(-log10(pval) > 5,
+                                           as.character(Variant), "")), aes(label = label), size = 3.5,
         label.size = NA, box.padding = 0.6, min.segment.length = 0.25) +
-    ggtitle("GBA1 variants associated with Parkinson's disease") +
     ylab(TeX("$-log_{10}p$")) +
-    xlab("Chromosome 1") +
+    xlab("Chromosome 1 Position") +
     labs(shape = "FDR", color = "") +
     theme(panel.grid.major.x = element_blank(),
           legend.key = element_rect(fill = "transparent"),
@@ -127,4 +62,5 @@ ggplot(summary, aes(x = Pos, y = -log10(pval))) +
                                             linetype = "dotted"),
           axis.line.y.left = element_line(color = "black"),
           axis.line.x.bottom = element_line(color = "black"))
-ggsave("ParkinsonManhattan.png")
+# Save
+ggsave("ParkinsonManhattan.png", dpi = 600)
